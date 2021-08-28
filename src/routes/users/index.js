5e7eb5a46799ebc12c0fb42cd02da341c3d610fd
lucaperullo <lucaperullo@outlook.it>
2021-08-28 17:21:23 +0200

    up

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -78,27 +78,24 @@ userRoutes.delete("/deleteUser", authorizeUser, async (req, res, next) => {
   }
 });
 
-userRoutes.post("/refresh", (req, res) => {
-  const refreshToken = req.body.refreshToken;
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken,
-  });
-
-  spotifyApi
-    .refreshAccessToken()
-    .then((data) => {
-      res.json({
-        accessToken: data.body.accessToken,
-        expiresIn: data.body.expiresIn,
+userRoutes.post(
+  "/refresh",
+  authorizeUser,
+  spotifyAuthentication,
+  (req, res) => {
+    req.spotifyApi
+      .refreshAccessToken()
+      .then((data) => {
+        res.json({
+          accessToken: data.body.accessToken,
+          expiresIn: data.body.expiresIn,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
       });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.sendStatus(400);
-    });
-});
+  }
+);
 
 export default userRoutes;
